90dfb349ab99a5e5f017b283e11080a4a01038d5
xnerhu <xnerhu@gmail.com>
2025-11-06 18:34:36 +0100

    t

--- service.ts
+++ service.ts
@@ -28,6 +28,4 @@ app.post("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Command runner listening on ${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => console.log(`listening on ${PORT}`));
